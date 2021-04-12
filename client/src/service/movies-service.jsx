d33b1dc653d0911fce0ac3d93173ac7a45c60cfd
mosmo46 <yasomoshe@gmail.com>
2021-04-12 14:32:11 +0300

    commit

--- client/src/service/movies-service.jsx
+++ client/src/service/movies-service.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+let PORT=process.env.PORT || 8080;
+
 export default async function getAllMovies() {
 
     try{
-        return await fetch('http://localhost:8080/movies/all')
+        return await fetch(`http://localhost:${PORT}/movies/all`)
         .then((res)=>{return res.json()})
         .then(results=>{return results.data})
     }catch(err){
