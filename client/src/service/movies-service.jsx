6fdb7059713f650cb2e0e8d69c952e41237512b5
shimitas <shmult@gmail.com>
2021-04-21 18:41:03 +0300

    commit

--- client/src/service/movies-service.jsx
+++ client/src/service/movies-service.jsx
@@ -1,13 +1,13 @@
 
 
-let PORT=process.env.PORT || 3000;
+// let PORT=process.env.PORT || 3000;
 
-// const API=process.env.NODE_ENV === 'production' ? `https://movies124.herokuapp.com` : `http://localhost:3000`;
+const API=process.env.NODE_ENV === 'production' ? `https://movies111.herokuapp.com` : `http://localhost:3000`;
 
 export default async function getAllMovies() {
 
     try{
-        return await fetch(`${PORT}/movies/all`)
+        return await fetch(`${API}/movies/all`)
         .then((res)=>{return res.json()})
         .then(results=>{return results.data})
     }catch(err){
